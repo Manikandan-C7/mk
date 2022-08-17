4f11af7f168f1507326e5e9d2ded746bbf5f3e9a
mk <mk@mk>
2022-08-17 09:46:38 +0530

    17/08/2022

---
+++ src/mk/Mani.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mk;
+
+/**
+ * @author mk
+ *
+ */
+public class Mani {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("ng");
+		
+	}
+
+}
---
+++ src/mk/bt.java
@@ -0,0 +1,5 @@
+package mk;
+
+public class bt {
+
+}
