9211fc7b831c74b3df55a4cf73de6cc7628ae832
mk <mk@mk>
2022-08-30 07:52:00 +0530

    spliting the int

--- src/mk/Mani.java
+++ src/mk/Mani.java
@@ -1,12 +1,27 @@
-/**
- * 
- */
+
+
 package mk;
 public class Mani {
 	public static void main(String[] args) {
-		int num=20;
-		num=40;
-		System.out.println("num is ----"+num);
+		int num=678;
+		
+		int a=num/100;
+		int b=num%100;
+		int c=b/10;
+		int d =b%10;
+		
+		
+	
+		
+		
+		
+		
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(d);
+		
+
+		
 		
 	}
 
