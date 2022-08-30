6f4255181856502038dc4cf84eade6d1171f050b
mk <mk@mk>
2022-08-30 09:02:45 +0530

    Swapping done by without extra variable.

---
+++ src/mk/Swapping.java
@@ -0,0 +1,31 @@
+package mk;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int x= 2;
+		int y=6;
+		int z=4;
+		
+		x=x+y;//x=2+6=8;
+	    y=x-y;//y=8-6=2;
+	    x=x-y;//x=8-2=6;
+	    //x=6; y=2;
+	    
+	    y=y+z; //y=2+4=6;
+	    z=y-z; //z=6-4=2;
+	    y=y-z; //y=6-2=4;
+	    //z=2
+		
+		
+		
+		
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+
+	}
+
+}
