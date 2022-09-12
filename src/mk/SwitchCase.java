36320795612b6a03865040d499754a1dbc37a645
mk <mk@mk>
2022-09-12 08:21:55 +0530

    Switch Case used to check "weekdays" or "weekend"..

---
+++ src/mk/SwitchCase.java
@@ -0,0 +1,25 @@
+package mk;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+	
+		int days=1;
+		
+	    switch (days) {
+	    case 1:
+	    case 2:
+	    case 3:	
+	    case 4:
+	    case 5:
+	    	System.out.println("WEEKDAYS");
+	    	break;
+	    case 6:
+	    case 7:
+	    	System.out.println("WEEKEND");
+	    	break;
+	    	
+
+	    }
+}
+}
