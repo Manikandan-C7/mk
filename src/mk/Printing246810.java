0fe031f31ed4f58982158875afceedba37232a98
mk <mk@mk>
2022-09-26 11:09:05 +0530

    PRINTING 246810

---
+++ src/mk/Printing246810.java
@@ -0,0 +1,15 @@
+package mk;
+
+public class Printing246810 {
+
+	public static void main(String[] args) {
+		int num=10;
+		for(int i=1;i<=num;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
