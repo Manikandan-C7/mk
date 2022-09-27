96405b415533fc39614c44843c3bca2a4a520dac
mk <mk@mk>
2022-09-27 20:15:43 +0530

    PRINTING ONLY MULTIPLIES OF 3 or  5

---
+++ src/mk/PrintingAssign12.java
@@ -0,0 +1,19 @@
+package mk;
+
+public class PrintingAssign12 {
+
+	public static void main(String[] args) {
+		// PRINTING ONLY MULTIPLIES OF 3 or  5
+		for(int i=1;i<=15;i++) {
+			if(i%3==0) {
+				System.out.println(i);
+			}
+			else if (i%5==0) {
+				System.out.println(i);
+			}
+				
+		}
+
+	}
+
+}
