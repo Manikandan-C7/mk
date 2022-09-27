da3d9d1a0197b2e897dfc53b59d100bb41105012
mk <mk@mk>
2022-09-27 20:24:00 +0530

    PRINTING 2 6 12 20 30

---
+++ src/mk/PrintingAssign13.java
@@ -0,0 +1,15 @@
+package mk;
+
+public class PrintingAssign13 {
+
+	public static void main(String[] args) {
+		// PRINTING 2 6 12 20 30
+		int n=1;
+		for(int i=2;i<=6;i++) {
+			System.out.println(n*i);
+			n++;
+		}
+
+	}
+
+}
