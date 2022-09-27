d1a97c280146a854a4372219c81dd0fbccb77801
mk <mk@mk>
2022-09-27 22:18:38 +0530

    Finding Prime Number

--- src/mk/PrintingAssign14.java
+++ src/mk/PrintingAssign14.java
@@ -4,7 +4,24 @@ public class PrintingAssign14 {
 
 	public static void main(String[] args) {
 		// Finding Prime Number
+		int num=11;
+		int c = 0;
+		for( int i=1;i<=num;i++) {
+			if(num%i==0) {
+				c++;
+			}
+		}
+		if(c==1 || c==2) {
+			System.out.println("PRIME");
+		}
+		else {
+			System.out.println("NOT PRIME");
+		}
+		
+			
+		
+		}
 
 	}
 
-}
+
