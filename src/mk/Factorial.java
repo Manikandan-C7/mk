f0a384717faf556230d41a840bd75284f9c0b9cc
mk <mk@mk>
2022-09-27 23:00:08 +0530

    Factorial of total

--- src/mk/Factorial.java
+++ src/mk/Factorial.java
@@ -3,12 +3,13 @@ package mk;
 public class Factorial {
 
 	public static void main(String[] args) {
-		// FACTORIAL
+		// Factorial 
 		int sum=1;
-		for(int i=1;i<=10;i++) {
+		for(int i=1;i<=5;i++) {
 			sum=sum*i;
-			System.out.println(sum );
+			
 		}
+		System.out.println(sum);
 
 	}
 
