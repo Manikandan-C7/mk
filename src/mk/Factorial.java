57f6a61f2360cd7906d1ff7125da78a12595d0d6
mk <mk@mk>
2022-09-27 22:51:20 +0530

    FACTORIAL

---
+++ src/mk/Factorial.java
@@ -0,0 +1,15 @@
+package mk;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// FACTORIAL
+		int sum=1;
+		for(int i=1;i<=10;i++) {
+			sum=sum*i;
+			System.out.println(sum );
+		}
+
+	}
+
+}
