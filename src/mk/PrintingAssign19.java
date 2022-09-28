439758e6de0c50213a944a2c2649eaeaf9d8d6f6
mk <mk@mk>
2022-09-28 11:33:14 +0530

    PRINTING 10 18 24 28...

---
+++ src/mk/PrintingAssign19.java
@@ -0,0 +1,18 @@
+package mk;
+
+public class PrintingAssign19 {
+
+	public static void main(String[] args) {
+		// PRINTING 10 18 24 28
+             int Num=10;
+             int i = 1;
+             while(i<=4) {
+            	 System.out.println(i*Num);
+            	 i++;
+            	 Num--;
+            	 
+             }
+
+	}
+
+}
