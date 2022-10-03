52028425477f7ad53f28bc90c46f2c5aa1248449
mk <mk@mk>
2022-10-03 15:04:46 +0530

    ArrayDeclaration

---
+++ src/Assignment/ArrayDeclaration.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+import java.util.Arrays;
+
+public class ArrayDeclaration {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+	   System.out.println(Arrays.toString(a));
+		
+		
+		
+	}
+
+}
