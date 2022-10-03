7c1d3fb5608216789b53c1e9dcc679c6f0bf287f
mk <mk@mk>
2022-10-03 15:29:48 +0530

    Printing Reverse order-Array

---
+++ src/Assignment/PrintingReverseOrder.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+import java.util.Arrays;
+
+public class PrintingReverseOrder {
+
+	public static void main(String[] args) {
+		//Printing Reverse order
+		
+		int a[]= {1,2,3,4,5};
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.print(a[i]);
+		}
+		
+		
+		
+
+	}
+
+}
