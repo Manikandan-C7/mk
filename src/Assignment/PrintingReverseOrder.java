ccc36fde7d6f11e4bc768a5c2c046b04d47610f4
mk <mk@mk>
2022-10-06 08:48:11 +0530

    Printing Reverse order in Array

--- src/Assignment/PrintingReverseOrder.java
+++ src/Assignment/PrintingReverseOrder.java
@@ -3,18 +3,20 @@ package Assignment;
 import java.util.Arrays;
 
 public class PrintingReverseOrder {
-
-	public static void main(String[] args) {
-		//Printing Reverse order
-		
-		int a[]= {1,2,3,4,5};
-		for(int i=a.length-1;i>=0;i--) {
-			System.out.print(a[i]);
+	void reverse(int a[]) {
+		for (int i=a.length-1;i>=0;i--) {
+			System.out.print(a[i]+" ");
 		}
 		
 		
-		
+	}
 
+	public static void main(String[] args) {
+		//Printing Reverse order
+		int a[]= {10,20,30,40,50};
+		PrintingReverseOrder ro=new PrintingReverseOrder();
+		ro.reverse(a);
+		
 	}
 
 }
