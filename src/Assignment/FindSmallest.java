e77f0b9894766f0942dd28ec2bc6f96174323d2e
mk <mk@mk>
2022-10-11 11:33:58 +0530

    12.FINDING FIRST TWO SMALLEST NUMBERS IN AN GIVEN ARRAY

--- src/Assignment/FindSmallest.java
+++ src/Assignment/FindSmallest.java
@@ -1,9 +1,26 @@
 package Assignment;
 
 public class FindSmallest {
+	
+	int smallest(int a[]) {
+		int max=a[0];
+		for(int i=0;i<a.length;i++) {
+			if(a[i]<max) {
+				max=a[i];
+				
+			}
+		}
+		return max;
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// FINDING SMALLEST NUMBER IN GIVEN ARRAY
+		int a[]= {10,3,7,20};
+		FindSmallest small=new FindSmallest();
+		int store=small.smallest(a);
+		System.out.println(store);
+		
 
 	}
 
